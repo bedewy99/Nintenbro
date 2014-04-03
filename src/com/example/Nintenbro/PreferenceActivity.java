9f28323a3f1703133e99de8d8543a3dfcd1c7e27
MehmetBaskoylu <baskoylu_mehmet@gmail.com>
2014-04-03 11:55:34 -0400

    Preference fragment PreferenceActivity Control Activity preference branch

---
+++ src/com/example/Nintenbro/PreferenceActivity.java
@@ -0,0 +1,13 @@
+package com.example.Nintenbro;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PreferenceActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ControllerActivity);
+    }
+}
