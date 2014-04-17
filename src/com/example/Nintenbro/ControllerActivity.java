17524f78a8c8774511390df136ab8bdabc42cd35
Sam Bedewy & Mehmet Bouskalba <sambedewy@yahoo.com>
2014-04-17 11:00:25 -0400

    Everything is about done

--- src/com/example/Nintenbro/ControllerActivity.java
+++ src/com/example/Nintenbro/ControllerActivity.java
@@ -1,7 +1,12 @@
 package com.example.Nintenbro;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class ControllerActivity extends Activity {
     /**
@@ -12,4 +17,27 @@ public class ControllerActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ControllerActivity);
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_action_bar, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Intent intent = new Intent(ControllerActivity.this, PreferenceActivity.class);
+                startActivity(intent);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
